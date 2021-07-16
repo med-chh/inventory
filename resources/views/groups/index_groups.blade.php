9db3dc26723677584a2fee2bcde51d6a5ff299d4
CesarObedFL <cesarobedfl@gmail.com>
2021-07-15 19:26:02 -0500

    update from laravel 5 to laravel 8

--- resources/views/groups/index_groups.blade.php
+++ resources/views/groups/index_groups.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 
+    <p>
+        LOS GRUPOS ERAN PARA DARLE ROLES Y PERMISOS A LOS USUARIOS, AL MODIFICAR UN DETERMINADO GRUPO, SE MODIFICARÍA EL GRUPO AL QUE PERTENECEN TODOS LOS USUARIOS RELACIONADOS
+    </p>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
